2994c039ec07b9de7aa7c88954a110c050171002
hp <apatil0695@gmail.com>
2022-12-21 09:38:25 +0530

    My first commit

---
+++ practice/src/main/java/com/practice/RegisterServlet1.java
@@ -0,0 +1,41 @@
+
+package com.practice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterServlet1
+ */
+@WebServlet("/RegisterServlet1")
+public class RegisterServlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.println("<h2>Welcome to Register Servlet</h2>");
+	 String name=request.getParameter("user_sport");
+	 String  country=request.getParameter("user_country");
+	 String player=request.getParameter("user_player");
+	 String gender=request.getParameter("user_gender");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
